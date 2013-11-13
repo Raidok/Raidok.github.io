13f3ddd790acb04f03a33b6e197ed550398bc471
Raidok <RKalbre@gmail.com>
2013-11-14 01:08:23 +0200

    peitsin väikese pühademuna headerisse

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$('.welcome').bind('dblclick', function(){
+  $(this).attr('contentEditable',true);
+}).blur(function(){
+  $(this).attr('contentEditable', false);
+  if (localStorage) localStorage.setItem('welcome', $(this).text());
+});
+$(setTimeout(function(){
+  var txt = localStorage.getItem('welcome') || 'hei';
+  $('.welcome').hide().text(txt).fadeIn('slow');
+}, 2000));
