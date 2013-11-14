0fd2b958e62a646fdf5b389f4aa7353a33d79533
Raidok <RKalbre@gmail.com>
2013-11-14 11:40:42 +0200

    tegin päise globaalseks, kodunupp

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ $('.welcome').bind('dblclick', function(){
   $(this).attr('contentEditable', false);
   if (localStorage) localStorage.setItem('welcome', $(this).text());
 });
-$(setTimeout(function(){
-  var txt = localStorage.getItem('welcome') || 'hei';
-  $('.welcome').hide().text(txt).fadeIn('slow');
-}, 2000));
+$(function() {
+	if ($('.welcome').text().length === 0)
+		setTimeout(function(){
+  		var txt = localStorage.getItem('welcome') || 'hei';
+  		$('.welcome').hide().text(txt).fadeIn('slow');
+		}, 2000)
+});
