37ad7726091dcb33f951bb9afb400c3b736e637d
Raidok <RKalbre@gmail.com>
2013-11-16 15:59:22 +0200

    enter-klahvi sidumine salvestamisega

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-$('.welcome').bind('dblclick', function(){
-  $(this).attr('contentEditable',true);
-}).blur(function(){
-  $(this).attr('contentEditable', false);
-  if (localStorage) localStorage.setItem('welcome', $(this).text());
-});
 $(function() {
+
+  // lazy loading
   $("img.lazy").show().lazyload({
     effect : "fadeIn",
     threshold : 200
   });
+
+  // easteregg
+  function save(that) {
+    $(that).attr('contentEditable', false);
+    if (localStorage) localStorage.setItem('welcome', $(that).text());
+  }
+
+  $('.welcome').bind('dblclick', function(){
+    $(this).attr('contentEditable',true);
+  }).blur(function(){
+    save(this);
+  }).keydown(function(e) {
+    if(e.keyCode == 13) {
+      save(this);
+      e.preventDefault();
+    }
+  });
+
   if ($('.welcome').text().length === 0)
     setTimeout(function(){
       var txt = localStorage.getItem('welcome') || 'hei';
