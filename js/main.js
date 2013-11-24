2d16f1f8cfcbea9dc03f9898ac239be5095a9b20
Raidok <RKalbre@gmail.com>
2013-11-24 21:30:48 +0200

    eestikeelsetel lehtedel kuvatakse inglisekeelsele avalehele suunamise teade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
+  // language suggestion
+  var language;
+  setTimeout(function(){$.ajax({ 
+      url: "http://ajaxhttpheaders.appspot.com", 
+      dataType: 'jsonp', 
+      success: function(headers) {
+          language = headers['Accept-Language'];
+          if (location.href.indexOf("/en/") == -1) {
+            if (language.indexOf("en;") != -1) {
+              $(".note").append('This blog is also available in English. Would You like to <a href="/en/">switch over</a>?');
+              $(".note").fadeIn('slow');
+            }
+          }
+      }
+  })}, 100);
+
   // lazy loading
   $("img.lazy").show().lazyload({
     effect : "fadeIn",
